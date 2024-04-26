02acfc8775f9faf5e7508542ebf1d54dc23e6678
Balraj0017 <95099000+Balraj0017@users.noreply.github.com>
2024-04-26 14:04:19 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,4 +5,8 @@ variable "sentinel_token" {}
 variable "client_id" {}
 variable "client_secret" {}
 variable "resource_group" {}
-variable "vm_names" {}
+variable "vm_names" {
+  type        = list(string)
+  default     = [ "xyz" ]
+  description = "resource group of vms to add sentinel one to"
+}
