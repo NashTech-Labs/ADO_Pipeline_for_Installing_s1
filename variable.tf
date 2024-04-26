785a12839cf7e905ba1d36e97d1deb6aeef6c8d0
Balraj0017 <95099000+Balraj0017@users.noreply.github.com>
2024-04-26 14:00:47 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "sentinel_apikey" {}
+variable "sentinel_token" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "resource_group" {}
+variable "vm_names" {}
